d3dd3fd1e746a47e64a8e6a637a51bbd5ea8d550
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-04-01 10:28:36 -0400

    Created Home Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "./App.css"
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+          <h2>Lambda Eats</h2>
+          <Link to="/">Home</Link>
+          <Link to="/about"> About</Link>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <h2>About Page</h2>
+          </Route>
+        </Switch>
+      </main>
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+
+const Home = () => {
+  return (
+    <>
+      <h3>Home22</h3>
+      <button id="order-pizza">Order Pizza</button>
+    </>
+  );
+};
+export default Home;
